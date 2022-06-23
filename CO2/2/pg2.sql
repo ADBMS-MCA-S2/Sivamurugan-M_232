97cea34666b6bdd2d05aea7227d76b4cd621da05
Sivamurugan <94527948+Siva-SMR@users.noreply.github.com>
2022-06-23 10:44:38 +0530

    Create pg2.sql

---
+++ CO2/2/pg2.sql
@@ -0,0 +1,6 @@
+create database emp;
+use emp;
+create table employee(emp_id INT,salary FLOAT);
+insert into employee values(1,20000);
+insert into employee values(110,10000);
+select emp_id,incent(emp_id),salary from employee;
